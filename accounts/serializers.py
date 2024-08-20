6480fd270b15bd0ed3ff8e2e89cd8d57ed257266
Saman-naruee <samannaruee@gmail.com>
2024-08-20 18:44:13 +0330

    make UserSerializer in accounts/serializers.py and UserRegistration in accounts/views.py

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-
+from django.contrib.auth.hashers import make_password
 from .models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'phone', 'address', 'gender', 'age', 'description', 'first_name', 'last_name', 'email']
+        extra_kwargs = {
+            'password' : {'write_only' : True, 'min_length' : 4}
+        }
+    def validate_password(self, pas):
+        return make_password(pas)
--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from .models import User
 
 from .serializers import UserSerializer
 
@@ -19,4 +20,15 @@ class LogoutAPIView(APIView):
 
 
 class UserRegistration(APIView):
-    pass
+    
+    def get(self, request):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data) # status=status.HTTP_201_CREATED
+        return Response(serializer.errors) # status=status.HTTP_400_BAD_REQUEST
